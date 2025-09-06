d6fe65bc245c3b76f3b3cc43220ba106bd916249
Mbolafy.Dev <mbolafy.dev@gmail.com>
2025-09-06 10:44:12 +0300

    settings

--- env_base_dir.py
+++ env_base_dir.py
@@ -1,8 +1,8 @@
 # env_base_dir.py
 # ⚠️ Ne jamais commiter ce fichier sur Git (ajoute-le à .gitignore)
 
-DB_NAME = "ZaraService$default"
-DB_USER = "ZaraService"
+DB_NAME = "zaraservice$default"
+DB_USER = "zaraservice"
 DB_PASSWORD = "Zara@586#"
 DB_HOST = "zaraservice.mysql.pythonanywhere-services.com"
 DB_PORT = "3306"
